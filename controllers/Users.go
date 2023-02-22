09d066fb4d22efa5659f4b3dd25f7b75fa686cad
lukitoandriansyah45 <lukitoandriansyah45@gmail.com>
2023-02-22 18:42:11 +0700

    add skeleton at file Users.go

--- controllers/Users.go
+++ controllers/Users.go
@@ -1,1 +1,31 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"task-5-vix-btpns-LUKITO_ANDRIANSYAH/helpers"
+)
+
+type UsersInterface interface {
+	Update(ctx *gin.Context)
+	Profile(ctx *gin.Context)
+}
+
+type UsersStruct struct {
+	usersHelperInterface helpers.UsersHelperInterface
+	jwtHelperInterface   helpers.JwtHelperInterface
+}
+
+func (us *UsersStruct) Update(ctx *gin.Context) {
+
+}
+
+func (us *UsersStruct) Profile(ctx *gin.Context) {
+
+}
+
+func NewUsersInterface(usersHelperInterfaceNew helpers.UsersHelperInterface, jwtHelperInterfaceNew helpers.JwtHelperInterface) UsersInterface {
+	return &UsersStruct{
+		usersHelperInterface: usersHelperInterfaceNew,
+		jwtHelperInterface:   jwtHelperInterfaceNew,
+	}
+}
