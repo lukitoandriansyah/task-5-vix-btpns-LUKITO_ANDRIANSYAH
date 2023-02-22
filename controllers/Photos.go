c778ac687fd98c6c956e2b0ef40b4371ac749c1c
lukitoandriansyah45 <lukitoandriansyah45@gmail.com>
2023-02-22 17:29:44 +0700

    add skeleton at file Photos.go

--- controllers/Photos.go
+++ controllers/Photos.go
@@ -1,13 +1,21 @@
 package controllers
 
-import (
-	"github.com/gin-gonic/gin"
-	"task-5-vix-btpns-LUKITO_ANDRIANSYAH/models"
-)
+import "task-5-vix-btpns-LUKITO_ANDRIANSYAH/helpers"
 
+/*
 func (idb *InDB) GetUser(c *gin.Context) {
 	var (
 		users  models.Users
 		result gin.H
 	)
+}*/
+
+type PhotosInterface interface {
+}
+
+type PhotosStruct struct {
+}
+
+func NewPhotosInterface(photosHelperInterfaceNew helpers.PhotosHelperInterface, jwtHelperInterfaceNew helpers.JwtHelperInterface) PhotosInterface {
+	return &PhotosStruct{}
 }
