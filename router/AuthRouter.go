c0b8f658f0d2b6b9e9240024fc36990e73e95720
lukitoandriansyah45 <lukitoandriansyah45@gmail.com>
2023-02-22 17:23:29 +0700

    add logic in method AuthRouter at file AuthRouter.go

--- router/AuthRouter.go
+++ router/AuthRouter.go
@@ -1,7 +1,9 @@
 package router
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	"task-5-vix-btpns-LUKITO_ANDRIANSYAH/controllers"
 	"task-5-vix-btpns-LUKITO_ANDRIANSYAH/helpers"
 	"task-5-vix-btpns-LUKITO_ANDRIANSYAH/models"
 )
@@ -11,8 +13,14 @@ var (
 	usersRepo           models.UsersRepo
 	authHelperInterface helpers.AuthHelperInterface
 	jwtHelperInterface  helpers.JwtHelperInterface
+	authInterface       controllers.AuthInterface
 )
 
 func AuthRouter() {
-
+	router := gin.Default()
+	authRoutes := router.Group("api/users")
+	{
+		authRoutes.POST("/login", authInterface.Login)
+		authRoutes.POST("/register", authInterface.Register)
+	}
 }
