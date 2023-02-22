4b8d0a6e27ff5181ac48cea4ba801ac974ce8693
lukitoandriansyah45 <lukitoandriansyah45@gmail.com>
2023-02-22 16:50:23 +0700

    add func jwtAuth

--- middleware/JwtAuth.go
+++ middleware/JwtAuth.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"task-5-vix-btpns-LUKITO_ANDRIANSYAH/helpers"
+)
+
+func JwtAuth(jwtHelperStruct helpers.JwtHelperStruct) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		authHeader := context.GetHeader("Authorization")
+		if authHeader == "" {
+			res := helpers.BuildErrorResponse("Sorry, failed to process data", "Roken not found", nil)
+			context.AbortWithStatusJSON(http.StatusUnauthorized, res)
+			return
+		}
+		token, err := jwtHelperStruct.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claim[userId]: ", claims["userId"])
+			log.Println("Claim[issuer] ", claims["issuer"])
+		} else {
+			log.Println(err)
+			res := helpers.BuildErrorResponse("Token is not valid", err.Error(), nil)
+			context.AbortWithStatusJSON(http.StatusUnauthorized, res)
+		}
+	}
+}
